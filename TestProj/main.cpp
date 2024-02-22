329562ea86dbba56e3d6af7324cca9a899ca22ec
a.shumeyko <a.shumeyko@socialquantum.com>
2024-02-22 21:21:28 +0300

    Сделал 2 домашнюю работу

--- TestProj/main.cpp
+++ TestProj/main.cpp
@@ -9,5 +9,18 @@ int main()
 	int var2 = 1;
 	std::cout << "va2 value: " << var2;
 
+	int var3;
+	std::cout << "Enter value";
+	std::cin >> var3;
+
+	if (var % 2 == 0)
+	{
+		std::cout << var << " can be divide to 2";
+	}
+	else
+	{
+		std::cout << var << " can't be divide to 2";
+	}
+
 	return 0;
 }
